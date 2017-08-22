f24ac78555ed0ab178a92cd0b4b62a6b9489e290
JacobAhlberg <jacahlberg@hotmail.com>
2017-08-22 16:09:33 +0200

    change cards depending on which value the user will get

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-    
     @IBOutlet weak var leftCard: UIImageView!
     @IBOutlet weak var rightCard: UIImageView!
     @IBOutlet weak var dealBtn: UIButton!
@@ -27,10 +25,26 @@ class ViewController: UIViewController {
     
     @IBAction func dealBtnPressed(_ sender: Any) {
         
+        let playerRandom: Int = Int(arc4random_uniform(13))
+        let CPURandom: Int = Int(arc4random_uniform(13))
+        
+        let arrayList = ["ace", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king"]
+        
+        let playerCard = arrayList[playerRandom]
+        let CPUCard = arrayList[CPURandom]
+        
+        leftCard.image = UIImage(named: playerCard)
+        rightCard.image = UIImage (named: CPUCard)
+        
+        if playerRandom > CPURandom || playerRandom == 0 && CPURandom != 0{
+//            playerScore += 1
+        } else if playerRandom == CPURandom{
+        } else{
+//            CPUScore += 1
+        }
+        
+        
     }
     
-    
-
-
 }
 
