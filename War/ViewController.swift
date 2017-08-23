71cf675045023e3d23dd18a85d40570dc16ed340
JacobAhlberg <jacahlberg@hotmail.com>
2017-08-23 12:26:55 +0200

    Full working app

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -15,15 +15,53 @@ class ViewController: UIViewController {
     @IBOutlet weak var dealBtn: UIButton!
     @IBOutlet weak var leftScore: UILabel!
     @IBOutlet weak var rightScore: UILabel!
+    @IBOutlet weak var winnerName: UILabel!
     
+    @IBOutlet var popUpView: UIView!
+    @IBOutlet weak var visualBlurView: UIVisualEffectView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Gömmer blureffekten
+        self.visualBlurView.isHidden = true
     }
-
+    
+    func animateIn(){
+        self.view.addSubview(popUpView)
+        popUpView.center = self.view.center
+        
+        popUpView.transform = CGAffineTransform.init(scaleX: 2, y: 3)
+        popUpView.alpha = 0
+        
+        UIView.animate(withDuration: 0.4) {
+            self.visualBlurView.isHidden = false
+            self.popUpView.alpha = 1
+            self.popUpView.transform = CGAffineTransform.identity
+        }
+    }
+    
+    func animateOut(){
+        UIView.animate(withDuration: 0.3, animations: {
+            self.popUpView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+            self.popUpView.alpha = 0
+            self.visualBlurView.isHidden = true
+        }) { (success:Bool) in
+            self.popUpView.removeFromSuperview()
+        }
+    }
+    
+    
+    var playerScore = 0
+    var CPUScore = 0
+    
+    
     
     @IBAction func dealBtnPressed(_ sender: Any) {
+        
+        if playerScore == 0 && CPUScore == 0{
+            self.timer()
+        }
         
         let playerRandom: Int = Int(arc4random_uniform(13))
         let CPURandom: Int = Int(arc4random_uniform(13))
@@ -37,13 +75,38 @@ class ViewController: UIViewController {
         rightCard.image = UIImage (named: CPUCard)
         
         if playerRandom > CPURandom || playerRandom == 0 && CPURandom != 0{
-//            playerScore += 1
+            playerScore += 1
+            leftScore.text = ("\(playerScore)")
         } else if playerRandom == CPURandom{
         } else{
-//            CPUScore += 1
+            CPUScore += 1
+            rightScore.text = ("\(CPUScore)")
         }
-        
-        
+    }
+    
+    func timer(){
+         _ = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(self.finishRound), userInfo: nil, repeats: false);
+    }
+    
+    @objc func finishRound() {
+        if playerScore > CPUScore{
+            winnerName.text = "Player!"
+        } else if (playerScore == CPUScore){
+            winnerName.text = "Nobody!"
+        } else{
+            winnerName.text = "CPU!"
+        }
+        self.animateIn()
+    }
+    
+    @IBAction func playAgainBtnPressed(_ sender: Any) {
+        leftCard.image = UIImage(named: "back")
+        rightCard.image = UIImage(named: "back")
+        playerScore = 0
+        CPUScore = 0
+        leftScore.text = "\(playerScore)"
+        rightScore.text = "\(CPUScore)"
+        self.animateOut()
     }
     
 }
